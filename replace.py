9be9fe59e58262394b17cb3b5ff9416790d94815
TayKara <tayfun1409@hotmail.fr>
2020-07-25 13:14:28 +0200

    The script do a better job

--- replace.py
+++ replace.py
@@ -1,6 +1,6 @@
 import os, sys, re
 
-userPath = "/Applications/Tunnelblick.app/Contents/user.txt"
+userPath = "/Users/taykara/user.txt"
 files = [f for f in os.listdir(sys.path[0]) if ".ovpn" in f]
 for f in files:
     print(f)
@@ -8,11 +8,16 @@ for f in files:
     fin = open(sys.path[0]+os.path.sep+f, "rt")
     #read file contents to string
     data = fin.read()
-    #replace all occurrences of the required string
+    #Add \n in the end of the string if there is not
+    if "\n" not in data[-1] :
+        data.join("\n")
+    #find option auth-user-pass and replace its value if there is one
     toReplace = re.search('auth-user-pass(.*)\n', data).group(1)
     print("toReplace : "+toReplace)
     data = data.replace("auth-user-pass"+toReplace, "auth-user-pass "+userPath)
-    data = data.replace("comp-lzo", "")
+    #remove comp-lzo preceded or followed by \n to remove the line
+    data = data.replace("\ncomp-lzo", "")
+    data = data.replace("comp-lzo\n", "")
     #close the input file
     fin.close()
     #open the input file in write mode
